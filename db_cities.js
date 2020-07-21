3926fc093f8d65261b934ff66b742dc23a636692
Mikhail Shuvarov <shyvarov@gmail.com>
2020-07-21 14:03:24 +0300

    feat: insain task completed

--- db_cities.js
+++ db_cities.js
@@ -10,7 +10,6 @@ const createConstructorCities = data => {
 		listAutocomplete = document.querySelector('.dropdown-lists__list--autocomplete');
 
 	listDefault.style.display = 'none';
-	const lang = 'RU';
 	button.style['pointer-events'] = 'none';
 	button.setAttribute('target', '_blank');
 
@@ -18,11 +17,11 @@ const createConstructorCities = data => {
 		arr.sort((a, b) => (+(a.count) > +(b.count) ? -1 : 1));
 	};
 
-	const createListDefault = (data, lang) => {
-		data[lang].forEach(item => {
+	const createListDefault = data => {
+		data.forEach(item => {
 			const div = document.createElement('div');
 			div.classList.add('dropdown-lists__countryBlock');
-			div.innerHTML = `<div class="dropdown-lists__total-line" data-country=${item.country}>
+			div.innerHTML = `<div class="dropdown-lists__total-line" data-country="${item.country}">
                                     <div class="dropdown-lists__country">${item.country}</div>
                                     <div class="dropdown-lists__count">${item.count}</div>
                                 </div>`;
@@ -40,7 +39,7 @@ const createConstructorCities = data => {
 		});
 	};
 
-	createListDefault(data, lang);
+	createListDefault(data);
 
 	const createListAutocomplete = (matches, searchText) => {
 		if (matches.length > 0) {
@@ -63,7 +62,7 @@ const createConstructorCities = data => {
 				const div = document.createElement('div');
 				div.classList.add('dropdown-lists__countryBlock');
 				const regexp = new RegExp(`^(${searchText})`, 'gi');
-				div.innerHTML = `<div class="dropdown-lists__total-line" data-country=${item.country}>
+				div.innerHTML = `<div class="dropdown-lists__total-line" data-country="${item.country}">
                                         <div class="dropdown-lists__country">${item.country.replace(regexp, `<em>$1</em>`)}</div>
                                         <div class="dropdown-lists__count">${item.count}</div>
                                     </div>`;
@@ -72,12 +71,12 @@ const createConstructorCities = data => {
 		}
 	};
 
-	const createListSelect = (data, lang, country) => {
-		data[lang].forEach(item => {
+	const createListSelect = (data, country) => {
+		data.forEach(item => {
 			if (item.country === country) {
 				const div = document.createElement('div');
 				div.classList.add('dropdown-lists__countryBlock');
-				div.innerHTML = `<div class="dropdown-lists__total-line" data-country=${item.country}>
+				div.innerHTML = `<div class="dropdown-lists__total-line" data-country="${item.country}">
                                         <div class="dropdown-lists__country">${item.country}</div>
                                         <div class="dropdown-lists__count">${item.count}</div>
                                     </div>`;
@@ -98,12 +97,12 @@ const createConstructorCities = data => {
 
 
 	const searchCities = searchText => {
-		const matchesCountry = data[lang].filter(item => {
+		const matchesCountry = data.filter(item => {
 			const regexp = new RegExp(`^${searchText}`, 'gi');
 			return item.country.match(regexp);
 		});
 		createListCountryAutocomplete(matchesCountry, searchText);
-		data[lang].forEach(item => {
+		data.forEach(item => {
 			const matches = item.cities.filter(city => {
 				const regex = new RegExp(`^${searchText}`, 'gi');
 				return city.name.match(regex);
@@ -125,10 +124,10 @@ const createConstructorCities = data => {
 			listDefault.style.transform = '';
 			listDefault.style.opacity = '';
 			listDefault.style.display = 'block';
-			listSelect.style.display = 'none';
 			listAutocomplete.style.display = 'none';
 			document.querySelector('.dropdown-lists__list--select .dropdown-lists__col').textContent = '';
 			listSelect.style.display = 'block';
+			listSelect.style.transform = '';
 		} else if (target.closest('.close-button')) {
 			selectCities.value = '';
 			listSelect.style.display = 'none';
@@ -153,7 +152,7 @@ const createConstructorCities = data => {
 			closeButton.style.display = 'block';
 			listDefault.style.transform = 'translateX(-100%)';
 			listDefault.style.opacity = 0;
-			createListSelect(data, 'RU', target.closest('.dropdown-lists__total-line').getAttribute('data-country'));
+			createListSelect(data, target.closest('.dropdown-lists__total-line').getAttribute('data-country'));
 			listSelect.style.transform = 'translateX(-100%)';
 			listSelect.style.opacity = 1;
 			selectCities.value = target.closest('.dropdown-lists__total-line').getAttribute('data-country');
@@ -176,7 +175,7 @@ const createConstructorCities = data => {
 			document.querySelector('.dropdown-lists__list--autocomplete .dropdown-lists__col').textContent = '';
 			listAutocomplete.style.display = 'none';
 		} else if (target.closest('.dropdown-lists__list--autocomplete .dropdown-lists__total-line')) {
-			createListSelect(data, 'RU', target.closest('.dropdown-lists__total-line').getAttribute('data-country'));
+			createListSelect(data, target.closest('.dropdown-lists__total-line').getAttribute('data-country'));
 			document.querySelector('.dropdown-lists__list--autocomplete .dropdown-lists__col').textContent = '';
 			listAutocomplete.style.display = 'none';
 			selectCities.value = target.closest('.dropdown-lists__total-line').getAttribute('data-country');
@@ -204,6 +203,22 @@ const createConstructorCities = data => {
 	});
 };
 
+
+const sortArr = (data, cookie) => {
+	if (cookie === 'RU' || cookie === 'EN' || cookie === 'DE') {
+		data.forEach((item, index) => {
+			if (item.country === 'Россия' || item.country === 'United Kingdom' || item.country === 'Deutschland') {
+				const newItem = item;
+				data.splice(index, 1);
+				data.unshift(newItem);
+			}
+		});
+	}
+};
+
+
+
+
 const createResponse = () => fetch('./db_cities.json', { method: 'GET', headers: { 'Content-Type': 'application/json' } });
 
 const createSpinner = () => {
@@ -222,22 +237,38 @@ const createSpinner = () => {
 	document.querySelector('.main').append(spinner);
 };
 
+let cookies = document.cookie.split("; ");
+cookies = cookies[0].split('=');
 
-createResponse().then(response => {
-	document.querySelector('.input-cities').style.display = 'none';
-	createSpinner();
-	if (response.status !== 200) {
-		throw new Error('status network not 200');
-	}
-	response.text().then(data => {
-		data = JSON.parse(data);
-		setTimeout(() => {
-			document.querySelector('#circularG').style.display = 'none';
-			document.querySelector('.input-cities').style.display = 'block';
-			createConstructorCities(data);
-		}, 3000);
+if (cookies[1] !== 'RU' && cookies[1] !== 'EN' && cookies[1] !== 'DE') {
+	createResponse().then(response => {
+		document.querySelector('.input-cities').style.display = 'none';
+		createSpinner();
+		const language = prompt('Выберите язык - RU, EN, DE').toUpperCase();
+		document.cookie = `lang=${language}; max-age=3600`;
+		document.querySelector('.input-cities').style.display = 'none';
+		cookies = document.cookie.split("; ");
+		cookies = cookies[0].split('=');
 
+		if (response.status !== 200) {
+			throw new Error('status network not 200');
+		}
+		response.text().then(data => {
+			data = JSON.parse(data);
+			localStorage.setItem('data', JSON.stringify(data[cookies[1]]));
+			data = data[cookies[1]];
+			sortArr(data, cookies[1]);
+			setTimeout(() => {
+				document.querySelector('#circularG').style.display = 'none';
+				document.querySelector('.input-cities').style.display = 'block';
+				createConstructorCities(data);
+			}, 3000);
+		});
+	}).catch(error => {
+		console.error(error);
 	});
-}).catch(error => {
-	console.error(error);
-});
+} else {
+	const dataLang = JSON.parse(localStorage.getItem('data'));
+	sortArr(dataLang, cookies[1]);
+	createConstructorCities(dataLang);
+}
